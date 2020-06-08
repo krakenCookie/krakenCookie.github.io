c99ce1e76c5ee7a8edcc4bcfc8afc95bcb2d5de1
burchill <zach.burchill+gh@gmail.com>
2020-06-08 17:36:28 -0400

    html deps to single dir

--- build.R
+++ build.R
@@ -19,21 +19,16 @@ local({
     dpi = 200, # Want the default dpi to be high
     fig.path   = sprintf('_posts/figures/generated/%s/', d),
     proj.basedir = getwd(),
-    plotly.loadpath = sprintf('/_posts/figures/generated/%s/', d),
-    plotly.savepath  = sprintf('%s/_posts/figures/generated/%s/', getwd(), d),
+    plotly.savepath  = sprintf('%s/_posts/figures/generated/html_dependencies/', getwd()),
     cache.path = sprintf('cache/%s/', d)
   )
 
   knitr::knit_hooks$set(autocaption = function(before, options, envir) {
-  if (!before) {
-  	if (length(options$fig.cap)>0) {
-  	  paste0("\n\n<p class='figcaption'>", options$fig.cap, "</p>")
-  	}
-  	#paste0(" WAAAA  ", str(options[c("fig.cap")]), "  sss  ", length(str(options[c("fig.cap")])), "mmmmm", options$fig.cap)
-    #z = capture.output(str(options[c('eval', 'dev', 'results', 'bar1', 'bar2', 'bar3')]))
-    #z = paste('    ', z, sep = '', collapse = '\n')
-    #paste('Some chunk options in the above chunk are:\n\n', z, sep = '')
-  }
+    if (!before && isTRUE(options$autocaption)) {
+      if (length(options$fig.cap)>0) {
+        paste0("\n\n<p class='figcaption'>", options$fig.cap, "</p>")
+      }
+    }
   })
   knitr::opts_knit$set(width = 70)
   knitr::knit(a[1], a[2], quiet = TRUE, encoding = 'UTF-8', envir = .GlobalEnv)
